b76bec6e0a1d6e1dcc3b14a32a7ddc170962be39
Elliot Barer <ebarer@mac.com>
2015-03-13 19:59:29 -0700

    Initial Commit

---
+++ QuickVite/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  QuickVite
+//
+//  Created by Elliot Barer on 2015-03-13.
+//  Copyright (c) 2015 Elliot Barer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
