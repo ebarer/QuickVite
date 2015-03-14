91c190db0126bd6e95ec16a204c1236b10c6fff4
Farzin Faghihi <ffaghihi@me.com>
2015-03-14 06:55:05 -0700

    adding changes for integration with Facebook SDK

---
+++ QuickVite/QVFriendsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QVFriendsViewController.swift
+//  QuickVite
+//
+//  Created by Farzin Faghihi on 2015-03-14.
+//  Copyright (c) 2015 Elliot Barer. All rights reserved.
+//
+
+import UIKit
+
+class QVFriendsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
