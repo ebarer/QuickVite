c41b8ae77cb8e1fa2462aaf880b7d4598d82df08
Gurinder Hans <hello@gurinderhans.me>
2015-03-14 09:29:55 -0700

    create invitation model / controller

---
+++ QuickVite/QVInvitation.swift
@@ -0,0 +1,22 @@
+//
+//  QVInvitation.swift
+//  QuickVite
+//
+//  Created by Gurinder Hans on 3/14/15.
+//  Copyright (c) 2015 Elliot Barer. All rights reserved.
+//
+
+import Foundation
+
+class QVInvitation {
+    
+    var invName: String
+    var invTime: String
+    
+    var people = [String]()
+    
+    
+    init(){
+        //
+    }
+}
