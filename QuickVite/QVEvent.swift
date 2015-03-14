76202ec5a68b4c40437b075f420fe5d0f1c6bfd5
Farzin Faghihi <ffaghihi@me.com>
2015-03-14 01:09:53 -0700

    added initial code for fetching and posting json data

--- QuickVite/QVEvent.swift
+++ QuickVite/QVEvent.swift
@@ -13,4 +13,30 @@ class QVEvent: NSObject {
     var name: String
     var date: NSDate
     var location: String
+    
+    
+    func getEvent() {
+        let urlAsString = "www.inserturlhere.com"
+        let url = NSURL(string: urlAsString)!
+        let request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), { (response, data, error) -> Void in
+            
+        })
+        
+    }
+    
+    func postEvent(data: NSData) {
+        let urlAsString = "www.inserturlhere.com"
+        let url = NSURL(string: urlAsString)!
+        
+        let request = NSMutableURLRequest(URL: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPMethod = "POST"
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(data, options: nil, error: nil)
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), { (response, data, error) -> Void in
+            
+        })
+        
+    }
 }
