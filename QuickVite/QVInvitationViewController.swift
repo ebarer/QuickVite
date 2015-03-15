cdea6784f26002be93a97e9d2b248fc50452e8c7
Elliot Barer <ebarer@mac.com>
2015-03-14 19:44:23 -0700

    + Storyboard fixed + Invitation implementation in progress

--- QuickVite/QVInvitationViewController.swift
+++ QuickVite/QVInvitationViewController.swift
@@ -9,21 +9,58 @@
 import UIKit
 
 class QVInvitationViewController: UITableViewController {
-    var itemList = ["Cat", "Bird", "Brick"]
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemList.count
+    var names = [AnyObject]()
+    var images = [String]()
+    
+    @IBOutlet var mTableView: UITableView!
+    
+    //async call for getting event invitations
+    func getInvitations() {
+        println("claeed get invitations")
+        let urlAsString = VQ.url + "/invitations"
+        let url = NSURL(string: urlAsString)!
+        let request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
+            
+            
+            var data = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: nil) as NSDictionary
+            if let data = data["people"]! as? [[String:AnyObject]] {
+                //                print(data.count)
+                for i in data {
+                    //                    println(i)
+                    self.names.append(i["name"]!)
+                }
+            }
+            self.mTableView.reloadData()
+            
+        })
     }
     
-//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCellWithIdentifier("ListViewCell", forIndexPath: indexPath) as UITableViewCell
-
-//        let item = itemList[indexPath.row]
-//
-//        cell.textLabel?.text = "test"
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-//        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
-//        
-//        }
-//    }
+        self.getInvitations()
+    }
+    
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.names.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("QVInvitationViewCell") as QVInvitationTableViewCell
+        
+        //        cell.textLabel?.text = self.names[indexPath.row] as String
+        
+        
+        return cell
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+    
 }
