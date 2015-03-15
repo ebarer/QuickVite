c50305b9c60e7c0b85e07662d9009c2cb2c3c922
Elliot Barer <ebarer@mac.com>
2015-03-14 20:18:11 -0700

    + Facebook auth. working + Friend list population corrected + Segue to QVEventVC started

--- QuickVite/QVTabBarViewController.swift
+++ QuickVite/QVTabBarViewController.swift
@@ -13,8 +13,11 @@ struct VQ {
 }
 
 var fbkLoggedIn: Bool = false
+var friendList = [String]()
 
-class QVTabBarViewController: UITabBarController {
+class QVTabBarViewController: UITabBarController, FBLoginViewDelegate {
+    
+    var fbLoginView = FBLoginView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,11 +31,32 @@ class QVTabBarViewController: UITabBarController {
             fbkLoggedIn = false
             self.selectedIndex = 3
         }
+        
+        fbLoginView.delegate = self
+        fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+        fbLoginView.loginBehavior = FBSessionLoginBehavior.UseSystemAccountIfPresent
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loginViewFetchedUserInfo(loginView: FBLoginView!, user: FBGraphUser!) {
+        let auth_token = FBSession.activeSession().accessTokenData.accessToken
+        let userID = user.objectID
+        
+        FBRequestConnection.startWithGraphPath("me/taggable_friends", completionHandler: {(connection, result, error) -> Void in
+            if let friends = result["data"] as? [[String:AnyObject]] {
+                for friend in friends {
+                    let name = friend["name"] as String
+                    friendList.append(name)
+                }
+                
+                var descriptor: NSSortDescriptor = NSSortDescriptor(key: "name", ascending: true, selector: "localizedStandardCompare:")
+                friendList = friendList.sorted{$0.localizedCaseInsensitiveCompare($1) == NSComparisonResult.OrderedAscending}
+            }
+        })
     }
 
 }
