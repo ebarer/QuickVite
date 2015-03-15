123cd065c1b07897237b2c9889f15f561252abbb
Gurinder Hans <hello@gurinderhans.me>
2015-03-14 17:39:56 -0700

    added multiple sub views to invitation table

---
+++ QuickVite/CustomTableCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableCell.swift
+//  QuickVite
+//
+//  Created by Gurinder Hans on 3/14/15.
+//  Copyright (c) 2015 Elliot Barer. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomTableCell: UITableViewCell{
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var label3: UILabel!
+}
--- QuickVite/QVInvitation.swift
+++ QuickVite/QVInvitation.swift
@@ -1,130 +1,42 @@
-////
-////  QVInvitation.swift
-////  QuickVite
-////
-////  Created by Gurinder Hans on 3/14/15.
-////  Copyright (c) 2015 Elliot Barer. All rights reserved.
-////
 //
-//import Foundation
+//  QVInvitation.swift
+//  QuickVite
 //
-//class QVInvitation {
-//        
-//    let invTo:String = ""
-//    let invFrom:String = ""
-//    let invTime:String = ""
+//  Created by Gurinder Hans on 3/14/15.
+//  Copyright (c) 2015 Elliot Barer. All rights reserved.
 //
-//    init(invTo: String, invFrom: String, invTime: String) {
-//        self.invTo = invTo
-//        self.invFrom = invFrom
-//        self.invTime = invTime
-//    }
-//    
-//    func getInvitations() {
-//        //fetch events
-//        let url = NSURL(string: VQ.url + "/persons")!
-//        let request = NSURLRequest(URL: url)
-//        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
-//            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options:nil, error: nil) as NSDictionary
-//            
-//        })
-//    }
-//    
-//    func createInvitaton(data: NSData) {
-//        let url = NSURL(string: VQ.url + "/invitation")!
-//        
-//        let request = NSMutableURLRequest(URL: url)
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.HTTPMethod = "POST"
-//        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(data, options: nil, error: nil)
-//        
-//        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
-//            
-//        })
-//        
-//    }
-//    
-//    func postEvent() {
-//        let url = NSURL(string: VQ.url + "/getNames")!
-//        let request = NSMutableURLRequest(URL: url)
-////
-////        var dateFormatter = NSDateFormatter()
-////        dateFormatter.dateFormat = "YYYY-MM-DD hh:mm" //format style. Browse online to get a format that fits your needs.
-////        var dateString = dateFormatter.stringFromDate(self.date!)
-////        
-////        var dict = ["type": self.type, "date": dateString]
-////        
-////        //checking if we have a location to had, if not it will crash passing in nil to dictionary
-////        if let loc = self.location {
-////            dict["location"] = loc
-////        }
-////        
-////        
-////        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-////        request.HTTPMethod = "POST"
-////        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(dict, options: nil, error: nil)
-////        
-////        
-////        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
-////            
-////        })
-//    }
-//}
-//
-//    init(invTo: String, invFrom: String, invTime: String) {
-//        self.invTo = invTo
-//        self.invFrom = invFrom
-//        self.invTime = invTime
-//    }
-//    
-//    func getInvitations() {
-//        //fetch events
-//        let url = NSURL(string: VQ.url + "/persons")!
-//        let request = NSURLRequest(URL: url)
-//        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
-//            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options:nil, error: nil) as NSDictionary
-//            
-//        })
-//    }
-//    
-//    func createInvitaton(data: NSData) {
-//        let url = NSURL(string: VQ.url + "/invitation")!
-//        
-//        let request = NSMutableURLRequest(URL: url)
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.HTTPMethod = "POST"
-//        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(data, options: nil, error: nil)
-//        
-//        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
-//            
-//        })
-//        
-//    }
-//    
-//    func postEvent() {
-//        let url = NSURL(string: VQ.url + "/getNames")!
-//        let request = NSMutableURLRequest(URL: url)
-////
-////        var dateFormatter = NSDateFormatter()
-////        dateFormatter.dateFormat = "YYYY-MM-DD hh:mm" //format style. Browse online to get a format that fits your needs.
-////        var dateString = dateFormatter.stringFromDate(self.date!)
-////        
-////        var dict = ["type": self.type, "date": dateString]
-////        
-////        //checking if we have a location to had, if not it will crash passing in nil to dictionary
-////        if let loc = self.location {
-////            dict["location"] = loc
-////        }
-////        
-////        
-////        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-////        request.HTTPMethod = "POST"
-////        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(dict, options: nil, error: nil)
-////        
-////        
-////        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
-////            
-////        })
-//    }
-//}
-//>>>>>>> gurinderhans
+
+import UIKit
+
+class QVInvitation {
+    
+    var userImage:UIImage,
+        userName:String,
+        type:String,
+        when:String
+    
+    init(userImage:UIImage, userName:String, type:String, when:String) {
+        self.userImage = userImage
+        self.userName = userName
+        self.type = type
+        self.when = when
+    }
+
+    class func getInvitations() {
+        var names = [AnyObject]()
+        let urlAsString = VQ.url + "/invitations"
+        let url = NSURL(string: urlAsString)!
+        let request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
+//            var data = NSString(data: data, encoding: NSUTF8StringEncoding)!
+            var data = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: nil) as NSDictionary
+            if let data = data["people"]! as? [[String:AnyObject]] {
+                for i in data {
+                    names.append(i)
+                }
+            }
+        })
+        
+    }
+
+}
