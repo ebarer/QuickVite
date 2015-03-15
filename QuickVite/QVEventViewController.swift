ab61c0328cf35f916fb534d1eb57e11a17d11465
Elliot Barer <ebarer@mac.com>
2015-03-15 02:07:49 -0700

    + QVEventController Complete

--- QuickVite/QVEventViewController.swift
+++ QuickVite/QVEventViewController.swift
@@ -8,15 +8,43 @@
 
 import UIKit
 
-class QVEventViewController: UIViewController {
-
+class QVEventViewController: UIViewController, UITableViewDataSource {
+    
+    var attendees:[QVPerson]?
     var aEvent: QVEvent?
-    @IBOutlet weak var testText: UILabel!
+    
+    @IBOutlet weak var eventName: UILabel!
+    @IBOutlet weak var eventDate: UILabel!
+    @IBOutlet weak var eventLocation: UILabel!
+    @IBOutlet weak var bgPhoto: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        println(aEvent?.type)
+        
+        if let event = aEvent {
+            self.title = event.type
+            eventName.text = event.type
+            
+            switch event.type {
+                case "Coffee":
+                    bgPhoto.image = UIImage(named: "Coffee")
+                case "Movies":
+                    bgPhoto.image = UIImage(named: "Movies")
+                default:
+                    bgPhoto.image = UIImage(named: "Other")
+            }
+            
+            self.navigationController?.navigationBar.barStyle = UIBarStyle.BlackTranslucent;
+            
+            var dateFormatter = NSDateFormatter()
+            dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+            dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+            eventDate.text = dateFormatter.stringFromDate(event.date!)
+            
+            eventLocation.text = event.location
+            
+    //        attendees = aEvent.getAttendees()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +52,19 @@ class QVEventViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let ppl = attendees {
+            return ppl.count
+        } else {
+            return 10
+        }
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("attendeeCell", forIndexPath: indexPath) as UITableViewCell
+//        cell.textLabel?.text = "\(attendees[indexPath.row].firstName) \(attendees[indexPath.row].lastName)"
+        cell.textLabel?.text = "Attendee"
+        return cell
+    }
 }
 
